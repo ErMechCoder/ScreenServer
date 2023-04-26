bff3e95e1e2944ee20414d1177dfbca56514c1dd
ErMechCoder <118120193+ErMechCoder@users.noreply.github.com>
2023-04-26 16:32:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ connection();
 
 
 
-// const User = mongoose.model('ScreenShort', {
-//   username: { type: String, required: true},
-//   type: { type: String, required: true},
-//   image: { type: String,  required: true},
-//   uploadedAt: {type: String},
-// });
+const User = mongoose.model('ScreenShort', {
+   usersId: { type: String, required: true},
+    type: { type: String, required: true},
+    image: { type: String,  required: true},
+    uploadedAt: {type: String},
+});
 
 // // Function call to Take window Image 
 
@@ -92,7 +92,7 @@ app.use("/api/user", engineerprofileRouter);
 app.use("/api/user",screenshotRoute)
 // app.use("/api",userscreenRouter)
 
-app.get("/api", async (req, res) => {
+app.get("/api/screen/get", async (req, res) => {
   const tasks = await   User.find()
   res.send(tasks)
 })
